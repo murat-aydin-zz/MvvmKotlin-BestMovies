cc090efdfe0a1420e900376ef387c6be83271ae9
Murat Aydın <murataydin@kukapps.com>
2019-09-04 17:22:57 +0300

    Bug fix

--- app/src/main/java/com/murat/movielist/ui/main/persondetail/PersonActivity.kt
+++ app/src/main/java/com/murat/movielist/ui/main/persondetail/PersonActivity.kt
@@ -1,26 +1,14 @@
 package com.murat.movielist.ui.main.persondetail
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Build
-import com.google.android.material.appbar.AppBarLayout
-import androidx.recyclerview.widget.GridLayoutManager
 import android.transition.Slide
 import android.view.Gravity
-import android.view.View
 import com.murat.movielist.BuildConfig
 import com.murat.movielist.R
 import com.murat.movielist.core.BaseActivity
-import com.murat.movielist.core.Resource
-import androidx.lifecycle.Observer
 
 import com.murat.movielist.databinding.ActivityPersonBinding
-import com.murat.movielist.service.response.Cast
-import com.murat.movielist.service.response.Person
-import com.murat.movielist.service.response.TMDBCreditsResponse
-import com.murat.movielist.ui.main.adapters.CastRecyclerViewAdapter
 import kotlinx.android.synthetic.main.activity_details.*
-import kotlin.math.abs
 
 
 class PersonActivity :  BaseActivity<PersonActivityViewModel, ActivityPersonBinding>(PersonActivityViewModel::class.java) {
